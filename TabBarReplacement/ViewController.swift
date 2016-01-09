f25bc3db3fa589764a10bd01c2f38c22b00e6445
Anthony Picciano <anthony.picciano@gmail.com>
2016-01-09 10:35:23 -0700

    Initial Commit

---
+++ TabBarReplacement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarReplacement
+//
+//  Created by Anthony Picciano on 1/9/16.
+//  Copyright © 2016 Anthony Picciano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
